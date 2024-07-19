4c1903eb14ecf86b119a088c9f88f534b187a6d5
Michael Shiloh <michael@michaelshiloh.com>
2024-07-19 17:33:24 +0400

    added another device

--- mainClock/mainClock.ino
+++ mainClock/mainClock.ino
@@ -3,23 +3,37 @@
     at HaMiffal
 
     TODO
-      fix when minutes roll over from 59 to 0
+      - fix when minutes roll over from 59 to 0
+      - make classes for each device as in the Adafruit Multitasking Tutorial
 */
 
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 #include "RTClib.h"
 
 // Pin definitions
-// Note that rtc uses SDA and SCL which is pins A4 and A5 so don't use them
 
 // outputs for relay board
-// NOTE THAT RELAY IS ACTIVE LOW
+// NOTE THAT RELAY IS ACTIVE LdOW
 const int FOURBAR_PIN = 2;
 const int unused3 = 3;
 const int unused4 = 4;
 const int CLICKY_PIN = 5;
-const int SPINNING_LAMP_LED_PIN = 6;
+
+// MOSFETs
+const int RATTLE_PIN = 6;
 const int SPINNING_LAMP_MOTOR_PIN = 7;
+
+// second relay board
+const int SECOND_RELAY_1 = 8;
+const int SECOND_RELAY_2 = 12;
+const int SECOND_RELAY_3 = 13;
+const int SECOND_RELAY_4 = A0;
+
+const int unusedA1 = A1;
+const int unusedA2 = A2;
+const int unusedA3 = A3;
+
+// Note that the RTC uses SDA and SCL which is pins A4 and A5 so don't use them
 
 // globals for recording time
 int lastMinute = 0;
@@ -35,9 +49,9 @@ void setup () {
   pinMode (CLICKY_PIN, OUTPUT);
   digitalWrite(CLICKY_PIN, HIGH); // active LOW
   pinMode (SPINNING_LAMP_MOTOR_PIN, OUTPUT);
-  digitalWrite(SPINNING_LAMP_MOTOR_PIN, LOW); 
-  pinMode (SPINNING_LAMP_LED_PIN, OUTPUT);
- analogWrite(SPINNING_LAMP_LED_PIN, 50); 
+  digitalWrite(SPINNING_LAMP_MOTOR_PIN, LOW);
+  pinMode (RATTLE_PIN, OUTPUT);
+  digitalWrite(RATTLE_PIN, LOW);
 
   Serial.begin(9600);
   Serial.println("Main clock for HaMiffal");
@@ -89,29 +103,30 @@ void loop () {
     lastMinute = 0;
   }
 
-  if (lastSeconds + 23 == now.second()) {
-    Serial.println("23 Seconds");
-    runClickeyThing();
-    lastSeconds = now.second();
-  }
-
   if (lastMinute + 1 == now.minute()) {
     Serial.println("One minute");
     runSpinningLamp();
+    delay(7000);
     runFourBar();
+    delay(14000);
+    runRattle();
+    delay(23000);
+    runClickeyThing();
     lastMinute = now.minute();
   }
 }
 
 void runSpinningLamp() {
 
+  Serial.println("Spinning Lamp");
   digitalWrite(SPINNING_LAMP_MOTOR_PIN, HIGH);
-  delay(10000);
-  digitalWrite(SPINNING_LAMP_MOTOR_PIN, LOW); 
+  delay(3000);
+  digitalWrite(SPINNING_LAMP_MOTOR_PIN, LOW);
 }
 
 void runFourBar() {
 
+  Serial.println("Four Bar");
   digitalWrite(FOURBAR_PIN, LOW); // active LOW
   delay(10000);
   digitalWrite(FOURBAR_PIN, HIGH); // active LOW
@@ -119,6 +134,7 @@ void runFourBar() {
 
 void runClickeyThing() {
 
+  Serial.println("Clicky Thing");
   digitalWrite(CLICKY_PIN, LOW); // active LOW
   delay(1000);
   digitalWrite(CLICKY_PIN, HIGH); // active LOW
@@ -130,6 +146,14 @@ void runClickeyThing() {
   digitalWrite(CLICKY_PIN, LOW); // active LOW
   delay(1000);
   digitalWrite(CLICKY_PIN, HIGH); // active LOW
+}
+
+void runRattle() {
+
+  Serial.println("Rattle");
+  digitalWrite(RATTLE_PIN, HIGH);
+  delay(7000);
+  digitalWrite(RATTLE_PIN, LOW);
 }
 
 void printTime() {
