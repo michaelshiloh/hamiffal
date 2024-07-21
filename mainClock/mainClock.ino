c9a590c02210404507078c0a60beb8ad22979d1c
Michael Shiloh <michael@michaelshiloh.com>
2024-07-21 15:36:39 +0400

    moved clicky thing to a class

--- mainClock/mainClock.ino
+++ mainClock/mainClock.ino
@@ -2,13 +2,14 @@
 /* Controller for Body of Work (גוף הזמן)
     at HaMiffal
 
-    TODO
-      - fix when minutes roll over from 59 to 0
-      - make classes for each device as in the Adafruit Multitasking Tutorial
-*/
+    Modification log
+    00 Jun 2024 - ms - initial entry; uses RTC
+    21 Jul 2024 - ms - don't use the RTC; instead
+                        make classes for each device
+                        as in the Adafruit Multitasking Tutorial
 
-// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
-#include "RTClib.h"
+
+*/
 
 // Pin definitions
 
@@ -34,20 +35,97 @@ const int unusedA2 = A2;
 const int unusedA3 = A3;
 
 // Note that the RTC uses SDA and SCL which is pins A4 and A5 so don't use them
+// Now I'm not using the RTC but it's still connected
 
-// globals for recording time
-int lastMinute = 0;
-int lastSeconds = 0;
+// Classes for each device
 
-RTC_DS1307 rtc;
+class ClickyThing
+{
+    int pinNumber;
+    int onTime = 1000;
+    int offTime = 1000;
+    int timeBetweenSequences = 20000;
+    int numberClicksPerSequence = 4;
+    int clickState = 0;
+    int inSequenceState = 0;
+    int clickCount = 0;
+    unsigned long lastClickStateChange = 0;
+    unsigned long lastSequenceStarted = 0;
+
+  public:
+    ClickyThing( int _pinNumber)
+    {
+      pinNumber = _pinNumber;
+      pinMode (pinNumber, OUTPUT);
+      digitalWrite(pinNumber, HIGH); // active LOW
+    }
+
+    void Update()
+    {
+      // we have two cases: either we are in a sequence, or we are between sequences
+
+      if (!inSequenceState) {
+        // we are between sequences
+
+        // Is it time to start a new sequence?
+        if ((millis() - lastSequenceStarted) > timeBetweenSequences)
+        {
+          // turn on click
+          digitalWrite(CLICKY_PIN, LOW); // active LOW
+
+          // update state
+          clickState = 1;
+          inSequenceState = 1;
+          lastClickStateChange = millis();
+          lastSequenceStarted = millis();
+          clickCount = 0;
+
+        }
+      } else { // otherwise, we are not between sequences
+        // so  we must be in the midst of a sequence
+        if (!clickState) {
+
+          // click is off so check whether it is time to turn it on
+          if ((millis() - lastClickStateChange) > offTime) {
+            // turn on click
+            digitalWrite(pinNumber, LOW); // active LOW
+
+            // update state
+            clickState = 1;
+            lastClickStateChange = millis();
+          }
+        } else {
+          // click is on so check whether it's time to turn it off
+          if ((millis() - lastClickStateChange) > onTime) {
+            // turn off click
+            digitalWrite(pinNumber, HIGH); // active LOW
+
+            // update state
+            clickState = 0;
+            lastClickStateChange = millis();
+
+            // since we finished a click, increment the click count
+            clickCount++;
+
+            // check to see if we're done with this sequence
+            if (clickCount > numberClicksPerSequence) {
+              // we're done
+              inSequenceState = 0;
+            }
+          }
+        }
+      }
+    }
+};
+
+ClickyThing clickyThing(CLICKY_PIN);
+
 
 void setup () {
 
   // Initialize outputs
   pinMode (FOURBAR_PIN, OUTPUT);
   digitalWrite(FOURBAR_PIN, HIGH); // active LOW
-  pinMode (CLICKY_PIN, OUTPUT);
-  digitalWrite(CLICKY_PIN, HIGH); // active LOW
   pinMode (SPINNING_LAMP_MOTOR_PIN, OUTPUT);
   digitalWrite(SPINNING_LAMP_MOTOR_PIN, LOW);
   pinMode (RATTLE_PIN, OUTPUT);
@@ -56,64 +134,14 @@ void setup () {
   Serial.begin(9600);
   Serial.println("Main clock for HaMiffal");
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (1) delay(10);
-  }
-
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-
-  // When time needs to be re-set on a previously configured device, the
-  // following line sets the RTC to the date & time this sketch was compiled
-  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  // This line sets the RTC with an explicit date & time, for example to set
-  // January 21, 2014 at 3am you would call:
-  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-
-  // Initialize the previous times
-  DateTime now = rtc.now();
-  lastMinute = now.minute();
-
   delay(100);
   Serial.println("setup finished");
 
 }
 
 void loop () {
-  DateTime now = rtc.now();
-  printTime();
 
-  if (lastSeconds >= now.second()) {
-    printTime();
-    Serial.println("Resetting seconds");
-    lastSeconds = 0;
-  }
-  if (1 == now.minute()) {
-    printTime();
-    Serial.println("Resetting minutes");
-    lastMinute = 0;
-  }
-
-  if (lastMinute + 1 == now.minute()) {
-    Serial.println("One minute");
-    runSpinningLamp();
-    delay(7000);
-    runFourBar();
-    delay(14000);
-    runRattle();
-    delay(23000);
-    runClickeyThing();
-    lastMinute = now.minute();
-  }
+  clickyThing.Update();
 }
 
 void runSpinningLamp() {
@@ -132,21 +160,7 @@ void runFourBar() {
   digitalWrite(FOURBAR_PIN, HIGH); // active LOW
 }
 
-void runClickeyThing() {
 
-  Serial.println("Clicky Thing");
-  digitalWrite(CLICKY_PIN, LOW); // active LOW
-  delay(1000);
-  digitalWrite(CLICKY_PIN, HIGH); // active LOW
-  delay(1000);
-  digitalWrite(CLICKY_PIN, LOW); // active LOW
-  delay(1000);
-  digitalWrite(CLICKY_PIN, HIGH); // active LOW
-  delay(1000);
-  digitalWrite(CLICKY_PIN, LOW); // active LOW
-  delay(1000);
-  digitalWrite(CLICKY_PIN, HIGH); // active LOW
-}
 
 void runRattle() {
 
@@ -154,14 +168,4 @@ void runRattle() {
   digitalWrite(RATTLE_PIN, HIGH);
   delay(7000);
   digitalWrite(RATTLE_PIN, LOW);
-}
-
-void printTime() {
-  DateTime now = rtc.now();
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
 }
